016f647082e47d6cf1db399fd3fccf2cba6a5425
groomigroom <googjohn0143@gmail.com>
2026-07-20 06:59:19 +0900

    Create inter.js

---
+++ Module_sample1/inter.js
@@ -0,0 +1,7 @@
+function showLogMessage(msg) {
+    console.log('------------------');
+    console.log(`로그 메시지는 : ${msg}`);
+    console.log('------------------');
+};
+
+module.exports.showLogMessage = showLogMessage
